356e4f5afc8d5af0c1bdf3671df452c8e9526081
Ryan Christian <rchristian@ryanchristian.dev>
2024-02-21 21:17:24 -0600

    chore: Add Preact example

---
+++ examples/preact/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+import { vitePrerenderPlugin } from 'vite-prerender-plugin';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		preact(),
+        vitePrerenderPlugin({
+            renderTarget: '#app',
+        }),
+	],
+});
