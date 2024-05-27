550b26ba35eec79130bb6924ce7033691e72ca6d
Ryan Christian <rchristian@ryanchristian.dev>
2024-05-27 18:01:46 -0500

    chore: Add React example

---
+++ examples/react/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { vitePrerenderPlugin } from 'vite-prerender-plugin';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        vitePrerenderPlugin({
+            renderTarget: '#root',
+        })
+    ],
+})
--- examples/vue/README.md
+++
@@ -1,3 +0,0 @@
-# Vue 3, Vite, and `@preact/preset-vite`'s prerenderer
-
-Demo showing painless & easy prerendering of Vue apps using the Preact Vite preset's prerender plugin
