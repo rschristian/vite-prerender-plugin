02d2f2f196953b0e7eac3c1b6c20118376ea64b2
Ryan Christian <rchristian@ryanchristian.dev>
2024-12-09 00:00:31 -0600

    fix: Incorrect destructure

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { previewMiddlewarePlugin } from './plugins/preview-middleware-plugin.js'
  * @param {import('./index.d.ts').Options} options
  * @returns {import('vite').Plugin[]}
  */
-export function vitePrerenderPlugin(options) {
+export function vitePrerenderPlugin(options = {}) {
     const { previewMiddlewareFallback, ...prerenderOptions } = options;
 
     return [
